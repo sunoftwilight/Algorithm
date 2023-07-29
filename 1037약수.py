31df4c5b2a9ea8c9bef00125dcc22d8b46c90a56
sun <v00417@naver.com>
2023-07-29 22:55:48 +0900

    0729

--- 1037약수.py
+++ 1037약수.py
@@ -1,10 +1,18 @@
+# T = int(input())
+
+# num_lst = list(map(int, input().split()))
+# num_lst = sorted(num_lst)
+
+# leng = len(num_lst)
+
+# origin = num_lst[0] * num_lst[leng-1]
+
+# print(origin)
+
 T = int(input())
 
 num_lst = list(map(int, input().split()))
-num_lst = sorted(num_lst)
 
-leng = len(num_lst)
-
-origin = num_lst[0] * num_lst[leng-1]
+origin = max(num_lst) * min(num_lst)
 
 print(origin)
