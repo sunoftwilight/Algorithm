4cd8efeba327c32ef5657a25ac0ed44006101935
sun <v00417@naver.com>
2023-09-11 17:28:26 +0900

    0911

---
+++ BOJ/2164카드2.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+N = int(input())
+card = deque()
+for i in range(1, N+1):
+    card.append(i)
+
+while len(card) > 1:
+    card.popleft()
+    card.append(card.popleft())
+
+print(card.popleft())
