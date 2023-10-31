8b6e8446482609e953c4b5418b06085d05e78ceb
sun <v00417@naver.com>
2023-10-31 23:13:54 +0900

    1031

--- BOJ/9489사촌.py
+++ BOJ/9489사촌.py
@@ -4,3 +4,22 @@ while True:
         break
 
     arr = list(map(int, input().split()))
+    parent = [0] * N
+    parent[0] = -1
+
+    idx = -1
+    for i in range(1, N):
+        if arr[i] == K:
+            target = i
+
+        if arr[i] != arr[i-1] + 1:
+            idx += 1
+
+        parent[i] = idx
+
+    ans = 0
+    for j in range(1, N):
+        if parent[j] != parent[target] and parent[parent[j]] == parent[parent[target]]:
+            ans += 1
+
+    print(ans)
