ade7907b3ba30186918c871976a2ed19777119bc
sun <129082968+sunoftwilight@users.noreply.github.com>
2024-04-04 13:46:04 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+num1 = input()
+num2 = input()
+
+out1 = int(num1) * int(num2[2])
+out2 = int(num1) * int(num2[1])
+out3 = int(num1) * int(num2[0])
+out4 = int(num1) * int(num2)
+
+print(out1)
+print(out2)
+print(out3)
+print(out4)
