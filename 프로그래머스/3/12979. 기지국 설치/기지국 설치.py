3698d3c517afd979945b6fb1e7c2b71f04d55691
sun <129082968+sunoftwilight@users.noreply.github.com>
2024-05-04 15:08:09 +0900

    Update 기지국 설치.py

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,17 +1,40 @@
-import math
-
-def solution(N, stations, W):
+def solution(n, stations, w):
     answer = 0
-    current_position = 1
-    station_index = 0
-    coverage_range = 2 * W + 1
 
-    while current_position <= N:
-        if station_index < len(stations) and stations[station_index] - W <= current_position:
-            current_position = stations[station_index] + W + 1
-            station_index += 1
-        else:
+    scopes = [(0, 0)]
+    for station in stations:
+        scopes.append((station - w, station + w))
+    scopes += [(n+1, n+1)]
+
+    cover = 2 * w + 1
+    for i in range(1, len(scopes)):
+        empty = scopes[i][0] - scopes[i - 1][1] - 1
+        if empty <= 0:
+            continue
+
+        share, remainder = divmod(empty, cover)
+        answer += share
+        if remainder:
             answer += 1
-            current_position += coverage_range
 
     return answer
+
+
+# by GPT
+# import math
+
+# def solution(N, stations, W):
+#     answer = 0
+#     current_position = 1
+#     station_index = 0
+#     coverage_range = 2 * W + 1
+
+#     while current_position <= N:
+#         if station_index < len(stations) and stations[station_index] - W <= current_position:
+#             current_position = stations[station_index] + W + 1
+#             station_index += 1
+#         else:
+#             answer += 1
+#             current_position += coverage_range
+
+#     return answer
