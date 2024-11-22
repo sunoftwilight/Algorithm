5fde7f4293fde288d07eb2bccc44bc2a1b54e5ed
sun <129082968+sunoftwilight@users.noreply.github.com>
2024-11-22 21:48:37 +0900

    [Silver III] Title: N과 M (1), Time: 136 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,7 @@
-def perm(arr, k):
-    if k >= M:
-        print(*arr)
-        return
-
-    for j in range(1, N+1):
-        if not visited[j]:
-            visited[j] = True
-            perm(arr+[j], k+1)
-            visited[j] = False
-
+from itertools import permutations
 
 N, M = map(int, input().split())
 visited = [False] * (N + 1)
 
-for i in range(1, N+1):
-    visited[i] = True
-    perm([i], 1)
-    visited[i] = False
+for perm in permutations(list(range(1, N+1)), M):
+    print(*list(perm))
