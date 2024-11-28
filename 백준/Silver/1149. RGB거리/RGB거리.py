e9af021d2379635a313a72638dc955dde8f85f30
sun <129082968+sunoftwilight@users.noreply.github.com>
2024-11-28 23:29:07 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -5,8 +5,8 @@ dp = [[0] * 3 for _ in range(N)]
 dp[0] = cost[0]
 
 for i in range(1, N):
-    dp[i][0] = min(dp[i-1][1] + cost[i][0], dp[i-1][2] + cost[i][0])
-    dp[i][1] = min(dp[i-1][0] + cost[i][1], dp[i-1][2] + cost[i][1])
-    dp[i][2] = min(dp[i-1][0] + cost[i][2], dp[i-1][1] + cost[i][2])
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2]
 
 print(min(dp[N-1]))
